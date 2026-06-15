134c7432fe85b47bcdf98c764b141d5586b06690
deborahfevlo <deborahfevlo@gmail.com>
2026-06-15 20:25:03 +0000

    refactor: reorganize page structure and extract reusable components

---
+++ src/pages/community/Community.tsx
@@ -0,0 +1,17 @@
+import HeroSection from "./components/HeroSection";
+import Channels from "./components/Channels";
+import CommunityGuidelines from "./components/CommunityGuidelines";
+import BottomCTA from "./components/BottomCTA";
+
+const Community = () => {
+  return (
+  <>
+    <HeroSection />
+    <Channels/>
+    <CommunityGuidelines/>
+    <BottomCTA/>
+  </>
+  );
+};
+
+export default Community;
